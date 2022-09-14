12e5142ae21d5f1e10a3ebc76a1590a06f29179f
Belinda Shan <belinda126@live.cn>
2022-09-14 14:21:33 +1000

    Q: move body with head

--- src/moduls/Snake.ts
+++ src/moduls/Snake.ts
@@ -19,6 +19,8 @@ class Snake {
 
   // set head new coordinate
   set X(value: number) {
+    this.moveBody();
+
     if (value < 0 || value > 290) {
       throw new Error("Hit the wall!");
     } else {
@@ -27,6 +29,8 @@ class Snake {
   }
 
   set Y(value: number) {
+    this.moveBody();
+
     if (value < 0 || value > 290) {
       throw new Error("Hit the wall!");
     } else {
@@ -36,6 +40,16 @@ class Snake {
 
   addBody = () => {
     this.element.insertAdjacentHTML("beforeend", "<div></div>");
+  };
+
+  // move body with head
+  moveBody = () => {
+    for (let i = this.body.length - 1; i > 0; i--) {
+      let X = (this.body[i - 1] as HTMLElement).offsetLeft;
+      let Y = (this.body[i - 1] as HTMLElement).offsetTop;
+      (this.body[i] as HTMLElement).style.left = X + "px";
+      (this.body[i] as HTMLElement).style.top = Y + "px";
+    }
   };
 }
 
